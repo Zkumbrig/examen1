66136e7837c81d3684386de82260d8d08652aeed
Zkumbrig <lveraperez6@gmai.com>
2023-04-19 16:28:37 -0500

    emfys

--- encabezado.php
+++ encabezado.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Conexión de PHP con PostgreSQL usando PDO">
     <meta name="author" content="Odenfis">
-    <title>PostgreSQL-PHP | Por Walter Moncada</title>
+    <title>PostgreSQL-PHP | Por Yoshi Vera </title>
     <!-- Cargar el CSS de Boostrap-->
     <link href="./bootstrap.min.css" rel="stylesheet">
     <!-- Cargar estilos propios -->
